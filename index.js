88e1f49d831f2235323588d2f34c6b2b4eac140c
Xilalus <62439697+Xilalus@users.noreply.github.com>
2020-03-20 17:46:39 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const { Client, Collection } = require('discord.js');
+const { token } = require('./2_config/botconfig.json');
+const bot = new Client();
+
+['commands', 'aliases'].forEach(x => bot[x] = new Collection);
+['command', 'events'].forEach(x => require(`./3_src/handlers/${x}`)(bot));
+
+bot.login(token);
