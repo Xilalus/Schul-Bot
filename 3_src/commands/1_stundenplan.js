cef72e3b650c6c1cd2c16cfa60f5d5214c325b7e
Xilalus <62439697+Xilalus@users.noreply.github.com>
2020-04-17 15:24:27 +0200

    Update 1_stundenplan.js

--- 3_src/commands/1_stundenplan.js
+++ 3_src/commands/1_stundenplan.js
@@ -1,8 +1,8 @@
-const { prefix, color, url } = require('../../2_config/botconfig.json')
+const { prefix, color } = require('../../2_config/botconfig.json')
 const { MessageEmbed } = require('discord.js')
 const MongoClient = require('mongodb').MongoClient
 
-url = process.env.url
+let url = process.env.url
 
 module.exports = {
 
