c58883c0185c5d73439f5f9cf4546baebcd395f5
Xilalus <62439697+Xilalus@users.noreply.github.com>
2020-04-17 19:53:38 +0200

    Update 1_befehle.js

--- 3_src/commands/1_befehle.js
+++ 3_src/commands/1_befehle.js
@@ -28,8 +28,8 @@ module.exports = {
             //Alle Befehle für Admin's und Moderatoren
             let embed = new MessageEmbed()
                 .setColor(color)
-                .setTitle('Befehle | Lehrer')
-                .setDescription('`!erstellen` - Hiermit können Sie einen neuen Stundenplan Eintrag erstellen\n \n`!stundenplan [jgs]` - Hiermit können Sie den Stundenplan der angegebenen Jahrgangstufe am aktuellen Tag aufrufen (`[jsg]` nur in Zahlenformat z.B. 11)\n \n`!stundeplan [jgs] [datum]` - Hiermit können Sie den Stundenplan der angegebenen Jahrgangstufe am angegebenen Tag aufrufen (`[jgs]` nur in Zahlenformat z.B. 11, `[datum]` im Format D.M.JJJJ)\n \n`!updatetimetable` - Hiermit werden alle heutigen Stundenpläne in die Kanäle dafür geschickt \n \n `!toggledaily [on/off]` - Hiermit können die täglichen Stundenplan Nachrichten an- bzw. ausgestellt werden \n \n`!rules` - Schickt die Regeln des Servers als Embed \n \n`!welcome` - Schickt die Willkommensnachricht als Embed \n \n`!delete [anzahl]` - Hiermit können Sie eine bestimmte Anzahl an Nachrichten köschen ')
+                .setTitle('Befehle | Admin/Moderator')
+                .setDescription('`!erstellen` - Hiermit können Sie einen neuen Stundenplan Eintrag erstellen\n \n`!stundenplan [jgs]` - Hiermit können Sie den Stundenplan der angegebenen Jahrgangstufe am aktuellen Tag aufrufen (`[jsg]` nur in Zahlenformat z.B. 11)\n \n`!stundeplan [jgs] [datum]` - Hiermit können Sie den Stundenplan der angegebenen Jahrgangstufe am angegebenen Tag aufrufen (`[jgs]` nur in Zahlenformat z.B. 11, `[datum]` im Format D.M.JJJJ)\n \n`!updatetimetable` - Hiermit werden alle heutigen Stundenpläne in die Kanäle dafür geschickt \n \n `!toggledaily [on/off]` - Hiermit können die täglichen Stundenplan Nachrichten an- bzw. ausgestellt werden \n \n`!rules` - Schickt die Regeln des Servers als Embed \n \n`!welcome` - Schickt die Willkommensnachricht als Embed \n \n`!delete [anzahl]` - Hiermit können Sie eine bestimmte Anzahl an Nachrichten löschen ')
             message.channel.send(embed)
 
         }
