7dc02acdb4ffdbc6a09d8f83d1e0201505568821
Xilalus <62439697+Xilalus@users.noreply.github.com>
2020-04-17 15:15:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Client, Collection } = require('discord.js');
+const { token } = require('./2_config/botconfig.json');
 const bot = new Client();
 
 ['commands', 'aliases'].forEach(x => bot[x] = new Collection);
