6c90c92fb94f9f3346672411f161c1db8bc2ef8a
Xilalus <62439697+Xilalus@users.noreply.github.com>
2020-04-17 14:22:54 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const { Client, Collection } = require('discord.js');
-const { token } = require('./2_config/botconfig.json');
-const bot = new Client();
-
-['commands', 'aliases'].forEach(x => bot[x] = new Collection);
-['command', 'events'].forEach(x => require(`./3_src/handlers/${x}`)(bot));
-
-bot.login(process.env.token);
