75150d0d28af027248138f7360b34b41c9c8d75a
Xilalus <62439697+Xilalus@users.noreply.github.com>
2020-04-17 15:20:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const bot = new Client();
 ['commands', 'aliases'].forEach(x => bot[x] = new Collection);
 ['command', 'events'].forEach(x => require(`./3_src/handler/${x}`)(bot));
 
-bot.login(token);
+bot.login(process.env.token);
