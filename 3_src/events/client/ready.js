27efa86041f832bb95f9d9fb2d8e06fc6ae587c6
Xilalus <62439697+Xilalus@users.noreply.github.com>
2020-05-03 17:53:57 +0200

    Update ready.js

--- 3_src/events/client/ready.js
+++ 3_src/events/client/ready.js
@@ -7,12 +7,7 @@ let url = process.env.url
 module.exports = async bot => {
     console.log(`${bot.user.username} is online! \nYou are logged in as ${bot.user.tag}`);
 
-    bot.user.setPresence({
-        game: {
-            name: '?befehle',
-            type: "PLAYING"
-        }
-    });
+    bot.user.setActivity('?befehle', { type: 'WATCHING' })
     
     let rule = new schedule.RecurrenceRule();
     rule.hour = [5];
